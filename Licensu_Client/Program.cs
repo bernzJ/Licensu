7b9dbe28cef3e89a7b63eeafb464d316e24bef7b
benJ <ben-0_0-@hotmail.com>
2017-08-20 03:20:10 -0400

    Added new object structure.

--- Licensu_Client/Program.cs
+++ Licensu_Client/Program.cs
@@ -13,7 +13,21 @@ namespace Licensu_Client
         static void Main(string[] args)
         {
             string key = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7InBsdWdpbiI6IlhoSnRCZkpLRzEuVDFuS2djckdjUC5kdFVUUFRuQUJLLlp4UmFkTU5YbmEuTXhJVWxsUk5WUy5VQ3lMREJYb3phLkhja2dCRlZ3RVgudmRkbVhiVlN3cy5QaHptYnNBR05GIiwiZGF5c0xlZnQiOiIzMCIsImJhbm5lZCI6ImZhbHNlOmZhbHNlIiwiTUQ1Ijoia2V5Ym9hcmQgY2F0IiwiSVBCQU4iOiIzOjEiLCJJUFMiOiIiLCJIV0lEIjoiZmFsc2UifSwiaWF0IjoxNTAyNTI5OTcwLCJleHAiOjE1MDUxMjE5NzB9.9gdEDoV5eh6putzHKPe1ww8CdoOVYZGvvGt1pxKtawY";
-            Core core = new Core(key, "clientcert.p12", "ca.crt", "xTurbo");
+
+            List<Crypto.CertificateObject> certificates = new List<Crypto.CertificateObject>();
+            certificates.Add(new Crypto.CertificateObject()
+            {
+                certificateFileExtension = ".ca",
+                certificateName = "ca",
+                // Cert data itself.
+                data = {},
+                // All caps, no space or -
+                Thumbprint = "",
+                isSSLAuth= false,
+
+            });
+
+            Core core = new Core(key, certificates, "xTurbo");
             core.remoteVariable = LoadMemory;
             iNotifAuth.StaticPropertyChanged += INotifAuth_StaticPropertyChanged;
             core.Authenticate();
